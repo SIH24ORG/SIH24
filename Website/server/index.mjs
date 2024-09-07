664443406a517575b55b15211526da32a48b7e62
Azaken1248 <suvkrm@gmail.com>
2024-09-07 14:17:38 +0530

    Added Main Server

--- Website/server/index.mjs
+++ Website/server/index.mjs
@@ -1,1 +1,17 @@
-//main server
+import express from 'express';
+import cors from 'cors';
+
+import paymentRouter from './routes/paymentRouter.mjs';
+import loginRouter from './routes/loginRouter.mjs';
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use('/loginApi',loginRouter);
+app.use('/payApi',paymentRouter);
+
+app.listen(3000, () => {
+    console.log('Server running on port 3000');
+});
