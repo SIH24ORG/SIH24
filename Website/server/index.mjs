547551f19454f0df6ea25e6d0c3971c1e6a642e6
Azaken1248 <suvkrm@gmail.com>
2024-09-09 02:55:00 +0530

    Changed Middleware use logic

--- Website/server/index.mjs
+++ Website/server/index.mjs
@@ -4,17 +4,18 @@ import cors from 'cors';
 import paymentRouter from './routes/paymentRouter.mjs';
 import loginRouter from './routes/loginRouter.mjs';
 
-import { analyzeTraffic } from './middleware/packetAnalyzer.mjs';
+import { startPacketCapture } from './middleware/packetAnalyzer.mjs';
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
-app.use(analyzeTraffic)
+
 
 app.use('/loginApi',loginRouter);
 app.use('/payApi',paymentRouter);
 
 app.listen(3000, () => {
     console.log('Server running on port 3000');
+    startPacketCapture();
 });
